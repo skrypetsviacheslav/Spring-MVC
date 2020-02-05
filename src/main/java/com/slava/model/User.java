b57d8b1cc1e19afb58ef976fce804df4621e5d1a
Viacheslav Skrypets <Viacheslav_Skrypets@epam.com>
2020-02-05 15:08:32 +0200

    fix: Validation - user email property is require

--- src/main/java/com/slava/model/User.java
+++ src/main/java/com/slava/model/User.java
@@ -2,6 +2,7 @@ package com.slava.model;
 
 import lombok.*;
 
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -11,11 +12,17 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 @Builder
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @NotBlank(message = "Name is required")
     private String name;
     @Size(min = 1, max = 10, message = "Surname should be from 1 to 10 symbol")
     private String surname;
+    @NotBlank(message = "Email is required")
     @Email
     private String email;
 }
